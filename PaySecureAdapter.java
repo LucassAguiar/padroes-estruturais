48c0bd4a836fbe4b828364d4a0c4d848fa99c650
LucassAguiar <75951975+LucassAguiar@users.noreply.github.com>
2026-07-05 22:22:25 -0300

    Create PaySecureAdapter.java

---
+++ PaySecureAdapter.java
@@ -0,0 +1,44 @@
+package br.unicamp.padroesestruturais.legacy.adapter;
+
+import br.unicamp.padroesestruturais.legacy.domain.FormaPagamento;
+import br.unicamp.padroesestruturais.legacy.domain.Pedido;
+import br.unicamp.padroesestruturais.legacy.domain.ResultadoCobranca;
+import br.unicamp.padroesestruturais.legacy.externo.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaySecureAdapter implements GatewayPagamento {
+
+    private final PaySecureGateway gateway = new PaySecureGateway();
+
+    @Override
+    public ResultadoCobranca cobrar(Pedido pedido, double valor, FormaPagamento forma) {
+        Map<String, Object> dados = new HashMap<>();
+        dados.put("orderId", pedido.getId());
+        dados.put("customerName", pedido.getCliente());
+        dados.put("amount", valor);
+        dados.put("currency", "BRL");
+
+        try {
+            TransacaoExterna transacao = gateway.processarTransacao(dados);
+            String status = transacao.getCodigoStatus() == 200 ? "APROVADA" : "RECUSADA";
+
+            return new ResultadoCobranca(
+                    pedido.getId(),
+                    valor,
+                    status,
+                    transacao.getReferenciaExterna(),
+                    forma
+            );
+        } catch (GatewayIndisponivelException e) {
+            return new ResultadoCobranca(
+                    pedido.getId(),
+                    valor,
+                    "RECUSADA",
+                    null,
+                    forma
+            );
+        }
+    }
+}
